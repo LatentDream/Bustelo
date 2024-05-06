62cab16df6407573967bc78a45dcc65870f7e7d2
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-05-05 20:39:58 -0400

    chore: 3D version of molido

--- src/molido.c
+++ src/molido.c
@@ -9,42 +9,9 @@
 #define MAP_SIZE 256
 
 
-void normaliseMap(MapType* map, int isLogScale) {
-
-    // Find max
-    int max = 0;
-    for (int i = 0; i < MAP_SIZE; i++) {
-        for (int j = 0; j < MAP_SIZE; j++) {
-            if ((*map)[i][j] > max) {
-                if (isLogScale) {
-                    max = logf((*map)[i][j]);
-                } else {
-                    max = (*map)[i][j];
-                }
-            }
-        }
-    }
-
-    // Normalize the map
-    for (int i = 0; i < MAP_SIZE; i++) {
-        for (int j = 0; j < MAP_SIZE; j++) {
-            float value;
-            if (isLogScale) {
-                value = logf((float)(*map)[i][j]) / max;
-            } else {
-                value = (float)(*map)[i][j] / max;
-            }
-            (*map)[i][j] = value * 255;
-        }
-    }
-
-}
-
-
 void writeMapToImage(const char* filename, MapType* normalizedMap) {
     // Write a map to a png file
     // Expect the map to be already normalize (int value between 0 & 255
-
     static uint32_t pixels[MAP_SIZE][MAP_SIZE] = {0};
     // Normalize the map
     for (int i = 0; i < MAP_SIZE; i++) {
@@ -60,7 +27,6 @@ void writeMapToImage(const char* filename, MapType* normalizedMap) {
 
 
 void parseArgs(int argc, char** argv, char** targetFile, int* isLogScale, int* startGui ) {
-
     // Print all args to the console
     printf("------ Welcome to Bustelo ------\n");
     // Check if "log" is passed
@@ -71,7 +37,6 @@ void parseArgs(int argc, char** argv, char** targetFile, int* isLogScale, int* s
         }
         if (strcmp(argv[i], "--gui") == 0) {
             // Set log scale flag to true
-            printf("GELOOO\n");
             *startGui = 1;
             return;
         }
@@ -95,7 +60,6 @@ void parseArgs(int argc, char** argv, char** targetFile, int* isLogScale, int* s
 
 
 void fillMap(char* targetFile, MapType* map) {
-
     FILE* fptr;
     fptr = fopen(targetFile, "rb");
     if (!fptr) {
@@ -119,10 +83,131 @@ void fillMap(char* targetFile, MapType* map) {
         (*map)[prevByte][currByte] += 1;
         prevByte = currByte;
     }
-
     if (ferror(fptr)) {
         printf("[ERROR] While reading file: %02X\n", currByte);  
     }
     fclose(fptr);
-
 }
+
+void fillMap3D(char* targetFile, Map3DType* map) {
+    FILE* fptr;
+    fptr = fopen(targetFile, "rb");
+    if (!fptr) {
+        printf("[ERROR] The file is not opened. Aborting the mission\n");
+        exit(0);
+    }
+
+    // Fie Operation
+    rewind(fptr);
+    
+    // Traverse the file
+    int init = 2;
+    int zByte;
+    int yByte;
+    int xByte;
+    while((xByte = fgetc(fptr)) != EOF) {
+        switch (init) {
+            case 2:
+                yByte = xByte;
+                init -= 1;
+                break;
+            case 1:
+                zByte = yByte;
+                yByte = xByte;
+                init -= 1;
+                break;
+            case 0:
+                (*map)[xByte][yByte][zByte] += 1;
+                zByte = yByte;
+                yByte = xByte;
+                break;
+        }
+    }
+
+    if (ferror(fptr)) {
+        printf("[ERROR] While reading file: %02X\n", xByte);  
+    }
+    fclose(fptr);
+}
+
+void normaliseMap(MapType* map, int isLogScale) {
+    // Find max
+    int max = 0;
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            if ((*map)[i][j] > max) {
+                if (isLogScale) {
+                    max = logf((*map)[i][j]);
+                } else {
+                    max = (*map)[i][j];
+                }
+            }
+        }
+    }
+    // Normalize the map
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            float value;
+            if (isLogScale) {
+                value = logf((float)(*map)[i][j]) / max;
+            } else {
+                value = (float)(*map)[i][j] / max;
+            }
+            (*map)[i][j] = value * 255;
+        }
+    }
+}
+
+void normaliseMap3D(Map3DType* map, int isLogScale) {
+    // Find max
+    int max = 0;
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            for (int k = 0; k < MAP_SIZE; k++) {
+                if ((*map)[i][j][k] > max) {
+                    if (isLogScale) {
+                        max = logf((*map)[i][j][k]);
+                    } else {
+                        max = (*map)[i][j][k];
+                    }
+                }
+            }
+        }
+    }
+    // Normalize the map
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            for (int k = 0; k < MAP_SIZE; k++) {
+                float value;
+                if (isLogScale) {
+                    value = logf((float)(*map)[i][j][k]) / max;
+                } else {
+                    value = (float)(*map)[i][j][k] / max;
+                }
+                (*map)[i][j][k] = value * 255;
+            }
+        }
+    }
+}
+
+
+void copyMap(MapType* orig, MapType* dest) {
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            (*dest)[i][j] = (*orig)[i][j];
+        }
+    }
+}
+
+void copyMap3D(Map3DType* orig, Map3DType* dest) {
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            for (int k = 0; k < MAP_SIZE; k++) {
+                (*dest)[i][j][k] = (*orig)[i][j][k];
+            }
+        }
+    }
+}
+
+
+
--- src/molido.h
+++ src/molido.h
@@ -4,11 +4,17 @@
 #define MAP_SIZE 256
 
 typedef int MapType[MAP_SIZE][MAP_SIZE];
+typedef int Map3DType[MAP_SIZE][MAP_SIZE][MAP_SIZE];
 
 void parseArgs(int argc, char** argv, char** targetFile, int* isLogScale, int* parseArgs);
 void writeMapToImage(const char* filename, MapType* normalizedMap);
+
 void fillMap(char* targetFile, MapType* map);
+void fillMap3D(char* targetFile, Map3DType* map);
+void copyMap(MapType* orig, MapType* dest);
+void copyMap3D(Map3DType* orig, Map3DType* dest);
 void normaliseMap(MapType* map, int isLogScale);
+void normaliseMap3D(Map3DType* map, int isLogScale);
 
 #endif // MOLIDO_H
 
